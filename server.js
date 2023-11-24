52c1fea013380d90691f749a3b51901b9214d66d
strayerlx <609903510@qq.com>
2023-11-24 22:04:50 +0800

    1

---
+++ server.js
@@ -0,0 +1,20 @@
+let http = require('http')
+
+let users = [
+    { id: 1, name: 'sherry1' },
+    { id: 2, name: 'sherry2' },
+]
+
+let server = http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if (req.url === '/api/users') {
+        res.end(JSON.stringify(users))
+    } else {
+        res.end('Not Found')
+    }
+
+})
+
+server.listen(3001, () => {
+    console.log('server success 3001')
+})
