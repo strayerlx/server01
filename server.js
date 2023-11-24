0162b0595a937c89d25b6d57732672199a536b50
strayerlx <609903510@qq.com>
2023-11-25 00:44:06 +0800

    1

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ let http = require('http')
 let users = [
     { id: 1, name: 'sherry1' },
     { id: 2, name: 'sherry2' },
+    { id: 3, name: 'sherry3' },
+    { id: 4, name: 'sherry4' },
+    { id: 5, name: 'sherry5' },
 ]
 
 let server = http.createServer(function (req, res) {
